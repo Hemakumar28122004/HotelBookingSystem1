289fdd49bb711728a27c688e22c7382d9055fcb5
Hemakumar28122004 <hemakumar4343@gmail.com>
2025-03-13 17:21:30 +0530

    Add files via upload
    sucesfully file added

---
+++ HotelbookingSystem/src/test/java/com/Hotel/room/HotelbookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Hotel.room;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelbookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
